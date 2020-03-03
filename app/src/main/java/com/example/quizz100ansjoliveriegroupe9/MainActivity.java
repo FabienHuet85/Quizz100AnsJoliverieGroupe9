4dc85a1ad2eba71f6c1b23d496723a9d2cc02b96
MaelFournier <mfournier2@la-joliverie.com>
2020-03-03 17:54:55 +0100

    Ajout de la bdd (ajout des classes permettant de créer les tables /!\ problèmes dans MainAtivity.java)

--- app/src/main/java/com/example/quizz100ansjoliveriegroupe9/MainActivity.java
+++ app/src/main/java/com/example/quizz100ansjoliveriegroupe9/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.quizz100ansjoliveriegroupe9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new SQLiteDataBaseHelper(this);
     }
 }
